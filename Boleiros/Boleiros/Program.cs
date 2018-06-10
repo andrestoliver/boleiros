79417db86f2839b4244b7d9d2c73766ca65c1783
André Oliveira <andre.stoliver@gmail.com>
2018-06-10 12:21:30 -0300

    Adicionando o .gitignore

---
+++ Boleiros/Boleiros/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Boleiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
