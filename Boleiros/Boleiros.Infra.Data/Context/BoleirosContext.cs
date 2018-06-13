52821f0bcd87801a87eb97429b7561e5662c7e72
André Oliveira <andre.stoliver@gmail.com>
2018-06-13 20:32:15 -0300

    Adição do UI

---
+++ Boleiros/Boleiros.Infra.Data/Context/BoleirosContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boleiros.Infra.Data.Context
+{
+    public class BoleirosContext : DbContext
+    {
+        public BoleirosContext(DbContextOptions options) : base(options)
+        { }
+
+
+    }
+}
