37537b2c4302bfee1ba24eed7bffddab141720c0
André Oliveira <andre.stoliver@gmail.com>
2018-06-13 23:42:04 -0300

    Testando commit direto

--- Boleiros/Boleiros.Infra.Data/EntityConfig/BolaoConfig.cs
+++ Boleiros/Boleiros.Infra.Data/EntityConfig/BolaoConfig.cs
@@ -11,23 +11,23 @@ namespace Boleiros.Infra.Data.EntityConfig
     {
         public void Configure(EntityTypeBuilder<Bolao> builder)
         {
-            #region Constraints
-            builder.ToTable("TB_BOLAO");
-            builder.HasKey(p => p.Id);
+            //#region Constraints
+            //builder.ToTable("TB_BOLAO");
+            //builder.HasKey(p => p.Id);
 
-            builder.HasOne(p => p.Campeonato)
-                   .WithMany(p => p.Boloes)
-                   .OnDelete(DeleteBehavior.Restrict);
+            //builder.HasOne(p => p.Campeonato)
+            //       .WithMany(p => p.Boloes)
+            //       .OnDelete(DeleteBehavior.Restrict);
 
-            builder.HasMany(p => p.PerfisUsuario)
-                   .WithOne(p => p.Bolao)
-                   .HasForeignKey("ID_BOLAO");
-            #endregion
+            //builder.HasMany(p => p.PerfisUsuario)
+            //       .WithOne(p => p.Bolao)
+            //       .HasForeignKey("ID_BOLAO");
+            //#endregion
 
-            #region Propertys
-            builder.Property(p => p.Id).HasColumnName("ID_BOLAO");
-            builder.Property(p => p.Nome).HasColumnName("ID_BOLAO");
-            #endregion
+            //#region Propertys
+            //builder.Property(p => p.Id).HasColumnName("ID_BOLAO");
+            //builder.Property(p => p.Nome).HasColumnName("ID_BOLAO");
+            //#endregion
         }
     }
 }
