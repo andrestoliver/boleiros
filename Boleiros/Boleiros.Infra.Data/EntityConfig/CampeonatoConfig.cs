930d5ccc9964a84e82010e3dff4fb122c218370d
André Oliveira <andre.stoliver@gmail.com>
2018-06-13 23:48:09 -0300

    Criação da configuração de campeonato

---
+++ Boleiros/Boleiros.Infra.Data/EntityConfig/CampeonatoConfig.cs
@@ -0,0 +1,17 @@
+﻿using Boleiros.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boleiros.Infra.Data.EntityConfig
+{
+    public class CampeonatoConfig : IEntityTypeConfiguration<Bolao>
+    {
+        public void Configure(EntityTypeBuilder<Bolao> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
