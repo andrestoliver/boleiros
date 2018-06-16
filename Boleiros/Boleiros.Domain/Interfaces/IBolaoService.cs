3fce832bc9564286fb5cabc8ed96a8b6edbbde05
André Oliveira <andre.stoliver@gmail.com>
2018-06-16 11:05:40 -0300

    Criando o Code First

---
+++ Boleiros/Boleiros.Domain/Interfaces/IBolaoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boleiros.Domain.Interfaces
+{
+    public interface IBolaoService
+    {
+
+    }
+}
---
+++ Boleiros/Boleiros.Infra.Data/Migrations/20180616131830_InitalMigration.cs
@@ -0,0 +1,270 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Boleiros.Infra.Data.Migrations
+{
+    public partial class InitalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Campeonatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campeonatos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Times",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Sigla = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Times", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposPerfil",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposPerfil", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Boloes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false),
+                    CampeonatoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boloes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Boloes_Campeonatos_CampeonatoId",
+                        column: x => x.CampeonatoId,
+                        principalTable: "Campeonatos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rodadas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Numero = table.Column<int>(nullable: false),
+                    Descricao = table.Column<string>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false),
+                    CampeonatoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rodadas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rodadas_Campeonatos_CampeonatoId",
+                        column: x => x.CampeonatoId,
+                        principalTable: "Campeonatos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PerfisUsuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UsuarioId = table.Column<int>(nullable: true),
+                    BolaoId = table.Column<int>(nullable: true),
+                    TipoPerfilId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PerfisUsuario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PerfisUsuario_Boloes_BolaoId",
+                        column: x => x.BolaoId,
+                        principalTable: "Boloes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PerfisUsuario_TiposPerfil_TipoPerfilId",
+                        column: x => x.TipoPerfilId,
+                        principalTable: "TiposPerfil",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PerfisUsuario_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jogos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IdMandante = table.Column<int>(nullable: false),
+                    IdVisitante = table.Column<int>(nullable: false),
+                    NumeroGolMandante = table.Column<int>(nullable: false),
+                    NumeroGolVisitante = table.Column<int>(nullable: false),
+                    IdRodada = table.Column<int>(nullable: false),
+                    RodadaId = table.Column<int>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jogos_Rodadas_RodadaId",
+                        column: x => x.RodadaId,
+                        principalTable: "Rodadas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Palpites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    JogoId = table.Column<int>(nullable: true),
+                    UsuarioId = table.Column<int>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Palpites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Palpites_Jogos_JogoId",
+                        column: x => x.JogoId,
+                        principalTable: "Jogos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Palpites_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Boloes_CampeonatoId",
+                table: "Boloes",
+                column: "CampeonatoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jogos_RodadaId",
+                table: "Jogos",
+                column: "RodadaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Palpites_JogoId",
+                table: "Palpites",
+                column: "JogoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Palpites_UsuarioId",
+                table: "Palpites",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PerfisUsuario_BolaoId",
+                table: "PerfisUsuario",
+                column: "BolaoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PerfisUsuario_TipoPerfilId",
+                table: "PerfisUsuario",
+                column: "TipoPerfilId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PerfisUsuario_UsuarioId",
+                table: "PerfisUsuario",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rodadas_CampeonatoId",
+                table: "Rodadas",
+                column: "CampeonatoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Palpites");
+
+            migrationBuilder.DropTable(
+                name: "PerfisUsuario");
+
+            migrationBuilder.DropTable(
+                name: "Times");
+
+            migrationBuilder.DropTable(
+                name: "Jogos");
+
+            migrationBuilder.DropTable(
+                name: "Boloes");
+
+            migrationBuilder.DropTable(
+                name: "TiposPerfil");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Rodadas");
+
+            migrationBuilder.DropTable(
+                name: "Campeonatos");
+        }
+    }
+}
